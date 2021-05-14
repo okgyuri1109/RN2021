6691b1f7d1bc2524d4b5e562226cceb7bbb6da05
okgyuri1109 <okgyuri1109@naver.com>
2021-05-14 15:15:25 +0900

    0514 수업 (예제 4-11)

--- app/App.js
+++ app/App.js
@@ -1,43 +1,49 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, StyleSheet, Image} from 'react-native';
 
-export default class App extends Component<{}> {
-    render() {
-      return (
-        <View style={styles.container}>
-           <Example style={{borderWidth: 1}}>    
-               <Text>borderWidth: 1</Text>
-           </Example>
-           <Example style={{borderWidth: 3, borderLeftWidth: 0}}>    
-               <Text>borderWidth: 3, borderLeftWidth: 0</Text>
-           </Example>
-           <Example style={{borderWidth: 3, borderLeftColor: 'red'}}>    
-               <Text>borderWidth: 3, borderLeftColor: 'red'</Text>
-           </Example>
-           <Example style={{borderLeftWidth: 3}}>    
-               <Text>borderLeftWidth: 3</Text>
-           </Example>
-           <Example style={{borderWidth: 1, borderStyle: 'dashed'}}>    
-               <Text>borderWidth: 1, borderStyle: 'dashed'</Text>
-           </Example>
-         </View>
-      );
-    }
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.cardContainer}>
+          <View style={styles.cardImageContainer}>
+            <Image style={styles.cardImage} source={require('./0514.png')} />
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
 
-const Example = (props) => (    
-    <View style={[styles.example,props.style]}>
-        {props.children}
-    </View>
-);
+const profileCardColor = 'dodgerblue';
 
-const styles = StyleSheet.create({ 
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    example: {
-        marginBottom: 15
-    }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardContainer: {
+    borderColor: 'black',
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderRadius: 20,
+    backgroundColor: profileCardColor,
+    width: 300,
+    height: 400,
+  },
+  cardImageContainer: {
+    backgroundColor: 'white',
+    borderWidth: 3,
+    borderColor: 'black',
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+  },
+  cardImage: {
+    width: 80,
+    height: 80,
+  },
 });
+
+export default App;
