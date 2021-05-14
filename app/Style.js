0fe37a2e13599e6d741e3d594214037804ec8172
okgyuri1109 <okgyuri1109@naver.com>
2021-05-14 14:07:55 +0900

    0514 예제4-8

--- app/Style.js
+++ app/Style.js
@@ -1,46 +1,23 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native'
 
-export const Colors = {    
-    dark: 'black',
-    light: 'white'
-};
+const styles = StyleSheet.create({    
+  container: {    
+    marginTop: 150,
+    backgroundColor: '#ededed',
+    flexWrap: 'wrap'
+  }
+})
 
-const baseContainerStyles = {    
+const buttons = StyleSheet.create({    
+  primary: {    
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-};
-
-const baseBoxStyles = {    
+    height: 70,
+    backgroundColor: 'red',
     justifyContent: 'center',
     alignItems: 'center',
-    borderWidth: 2,
-    height: 150,
-    width: 150
-};
+    marginLeft: 20,
+    marginRight: 20
+  }
+})
 
-const lightStyleSheet = StyleSheet.create({    
-    container: {
-        ...baseContainerStyles,
-        backgroundColor: Colors.light
-    },
-    box: {
-        ...baseBoxStyles,
-        borderColor: Colors.dark
-    }
-});
-
-const darkStyleSheet = StyleSheet.create({    
-    container: {
-        ...baseContainerStyles,
-        backgroundColor: Colors.dark
-    },
-    box: {
-        ...baseBoxStyles,
-        borderColor: Colors.light
-    }
-});
-
-export default function getStyleSheet(useDarkTheme){    
-    return useDarkTheme ? darkStyleSheet : lightStyleSheet;    
-}
+export { styles, buttons }    
--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
+ import react from 'react';
  import {AppRegistry} from 'react-native';
  import App from './app/App';
  import {name as appName} from './app.json';
+ const myComponentTest = () => <App />
  
  AppRegistry.registerComponent(appName, () => App);
